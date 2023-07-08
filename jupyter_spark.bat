8d4963051cd848c3e1319c41e01d80c3692b4636
Scattolin-Marco <scattolin.marco@bcg.com>
2023-07-08 17:31:58 +0200

    initial commit

--- jupyter_spark.bat
+++ jupyter_spark.bat
@@ -1,4 +1,6 @@
+@echo copying local.yaml in .venv/Lib/local.yaml
 @echo off
+copy local.yaml ".venv/Lib/local.yaml"
 set PYSPARK_DRIVER_PYTHON=.\.venv\Scripts\jupyter-notebook.exe
 call pyspark --packages io.delta:delta-core_2.12:1.2.1 --conf "spark.sql.extensions=io.delta.sql.DeltaSparkSessionExtension" --conf "spark.sql.catalog.spark_catalog=org.apache.spark.sql.delta.catalog.DeltaCatalog"
 pause
