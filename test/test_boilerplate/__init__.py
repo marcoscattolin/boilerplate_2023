d8c7c3cc4caae76a80a0923c5d1a8537a0f3cd60
Scattolin-Marco <scattolin.marco@bcg.com>
2023-07-08 16:29:13 +0200

    initial commit

--- test/test_boilerplate/__init__.py
+++ test/test_boilerplate/__init__.py
@@ -3,7 +3,5 @@
 # ============================================================
 __copyright__ = "Copyright (C) 2022, Boston Consulting Group"
 __license__ = "Proprietary"
-__author__ = (
-    "Marco Scattolin <scattolin.marco@bcg.com>",
-)
+__author__ = ("Marco Scattolin <scattolin.marco@bcg.com>",)
 # ============================================================
