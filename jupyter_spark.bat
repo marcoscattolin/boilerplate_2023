b2925aa8e69ce6a26ede675a15f4cd0b79859281
Scattolin-Marco <scattolin.marco@bcg.com>
2023-07-08 15:39:16 +0200

    initial commit

---
+++ jupyter_spark.bat
@@ -0,0 +1,4 @@
+@echo off
+set PYSPARK_DRIVER_PYTHON=.\.venv\Scripts\jupyter-notebook.exe
+call pyspark --packages io.delta:delta-core_2.12:1.2.1 --conf "spark.sql.extensions=io.delta.sql.DeltaSparkSessionExtension" --conf "spark.sql.catalog.spark_catalog=org.apache.spark.sql.delta.catalog.DeltaCatalog"
+pause
