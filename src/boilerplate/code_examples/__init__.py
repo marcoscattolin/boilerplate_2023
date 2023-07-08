4eefb8974908336ef0d473a3a20f4435a2f37fb7
Scattolin-Marco <scattolin.marco@bcg.com>
2023-07-08 15:57:21 +0200

    initial commit

---
+++ src/boilerplate/code_examples/__init__.py
@@ -0,0 +1,7 @@
+# ============================================================
+# COPYRIGHT
+# ============================================================
+__copyright__ = "Copyright (C) 2022, Boston Consulting Group"
+__license__ = "Proprietary"
+__author__ = ("Marco Scattolin <scattolin.marco@bcg.com>",)
+# ============================================================
