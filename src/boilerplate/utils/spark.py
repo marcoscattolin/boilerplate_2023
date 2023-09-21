d040bf3f543f0b6ce7443bd9a38e01bc402b0336
Scattolin-Marco <scattolin.marco@bcg.com>
2023-09-21 17:43:03 +0200

    included delta support

--- src/boilerplate/utils/spark.py
+++ src/boilerplate/utils/spark.py
@@ -7,7 +7,15 @@ from pyspark.sql import SparkSession, SQLContext
 
 def spark_init():
     _spark = (
-        SparkSession.builder.appName("boilerplate_2023").master("local[*]").getOrCreate()
+        SparkSession.builder.appName("spark_env")
+        .config("spark.jars.packages", "io.delta:delta-core_2.12:1.2.1")
+        .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension")
+        .config(
+            "spark.sql.catalog.spark_catalog",
+            "org.apache.spark.sql.delta.catalog.DeltaCatalog",
+        )
+        .master("local[*]")
+        .getOrCreate()
     )
 
     sc = _spark.sparkContext
